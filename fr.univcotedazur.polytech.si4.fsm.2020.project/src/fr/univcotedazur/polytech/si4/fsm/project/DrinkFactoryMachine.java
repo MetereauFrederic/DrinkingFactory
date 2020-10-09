9d2608edd10cd72ff5c5842763cd9b2486d46e88
LudovicMarti <ludovic.marti@etu.unice.fr>
2020-10-09 10:24:03 +0200

    implémentation du squelette de la fsm

--- fr.univcotedazur.polytech.si4.fsm.2020.project/src/fr/univcotedazur/polytech/si4/fsm/project/DrinkFactoryMachine.java
+++ fr.univcotedazur.polytech.si4.fsm.2020.project/src/fr/univcotedazur/polytech/si4/fsm/project/DrinkFactoryMachine.java
@@ -33,7 +33,8 @@ public class DrinkFactoryMachine extends JFrame {
 	/**
 	 * @wbp.nonvisual location=311,475
 	 */
-	private final ImageIcon imageIcon = new ImageIcon();
+	
+	//private final ImageIcon imageIcon = new ImageIcon();
 
 	/**
 	 * Launch the application.
